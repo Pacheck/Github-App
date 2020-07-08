9bc23383e199be9ccf14ac02cb73322a81f374c6
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-07-08 13:13:18 -0300

    atualizando spread

--- src/App.js
+++ src/App.js
@@ -78,10 +78,7 @@ class App extends Component {
   render() {
     return (
       <AppContent
-        userinfo={this.state.userinfo}
-        repos={this.state.repos}
-        starred={this.state.starred}
-        isFetching={this.state.isFetching}
+        {...this.state}
         handleSearch={(e) => this.handleSearch(e)}
         getRepos={this.getRepos('repos')}
         getStarred={this.getRepos('starred')}
