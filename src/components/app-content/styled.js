02efc634dadda635f0c9338ce9365512186bd633
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-07-13 01:34:20 -0300

    mínimos ajuestes no style

--- src/components/app-content/styled.js
+++ src/components/app-content/styled.js
@@ -6,13 +6,20 @@ const App = styled.div`
   max-width: 800px;
   min-width: 325px;
   padding: 25px;
-  background-color: blanchedalmond;
+  background-color: #ddd7d7;
   border-radius: 15px;
 
   .repo-flex {
     margin: 0 auto;
     display: flex;
     padding: 10px;
+  }
+
+  @media (max-width: 515px) {
+    .repo-flex {
+      display: grid;
+      grid-template-columns: 1fr;
+    }
   }
 `;
 
--- src/components/search/styles.js
+++ src/components/search/styles.js
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
 
 const InputSearch = styled.div`
+  width: 80%;
   margin: 0 auto;
-  input {
-    height: 30px;
 
+  input {
+    width: 100%;
+    height: 35px;
     border-radius: 5px;
     box-sizing: content-box;
   }
