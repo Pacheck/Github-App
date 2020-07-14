0145b55ace82896b3b356e11de1a4ff0996d3abb
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-07-14 01:56:55 -0300

    ajustes CSS

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,19 +1,30 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-
 *{
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-}
+};
+
+    #root {
+        display: flex;
+        flex: 1;
+        align-items: center;
+        justify-content: center;
+        max-width: 800px;
+        min-width: 324px;
+    }
+
 
 body {  
     color: ${(props) => (props.isDark ? '#000' : '#fff')};
     background-color: ${(props) => (props.isDark ? '#c4baba' : '#121212')};
-
-    justify-items: center;
-
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    max-width: 800px;
+    min-width: 250px;
 }`;
 
 export default GlobalStyle;
