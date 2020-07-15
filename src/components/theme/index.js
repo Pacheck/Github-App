e8221610bdc6b6d901f354233333713c1df9f1c6
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-07-15 04:10:45 -0300

    Input do dark theme

--- src/components/theme/index.js
+++ src/components/theme/index.js
@@ -1,5 +1,25 @@
+// import React from 'react';
+
+// const Switch = ({ hClick }) => <input type="checkbox" onClick={hClick} />;
+
+// export default Switch;
+// //
+
 import React from 'react';
+import './index.css';
 
-const Tema = ({ hClick }) => <input type="checkbox" onClick={hClick} />;
+const Switch = ({ hClick }) => (
+  <>
+    <input
+      className="react-switch-checkbox"
+      id={`react-switch-new`}
+      type="checkbox"
+      onClick={hClick}
+    />
+    <label className="react-switch-label" htmlFor={`react-switch-new`}>
+      <span className={`react-switch-button`} />
+    </label>
+  </>
+);
 
-export default Tema;
+export default Switch;
