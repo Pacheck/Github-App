5d244836f98eed6df1217fb948fee90a8aaeac1a
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-07-16 04:39:42 -0300

    adding the props to change the button background color

--- src/components/actions/index.js
+++ src/components/actions/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Botao, Act } from './styles';
 
-const Actions = ({ getRepos, getStarred }) => (
+const Actions = ({ getRepos, getStarred, isDark }) => (
   <Act className="actions">
-    <Botao onClick={getRepos}>Ver repositórios</Botao>
-    <Botao onClick={getStarred}>Ver favoritos</Botao>
+    <Botao onClick={getRepos} isDark={isDark}>
+      Ver repositórios
+    </Botao>
+    <Botao onClick={getStarred} isDark={isDark}>
+      Ver favoritos
+    </Botao>
   </Act>
 );
 
