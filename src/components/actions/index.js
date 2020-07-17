bd12d8427535165ffd76527a6fc6c3115ef65725
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-07-17 03:18:52 -0300

    creating divider

--- src/components/actions/index.js
+++ src/components/actions/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { Botao, Act } from './styles';
+import { Botao, Act, Hr } from './styles';
 
 const Actions = ({ getRepos, getStarred, isDark }) => (
-  <Act className="actions">
-    <Botao onClick={getRepos} isDark={isDark}>
-      Ver repositórios
-    </Botao>
-    <Botao onClick={getStarred} isDark={isDark}>
-      Ver favoritos
-    </Botao>
-  </Act>
+  <>
+    <Act className="actions">
+      <Botao onClick={getRepos} isDark={isDark}>
+        Ver repositórios
+      </Botao>
+      <Botao onClick={getStarred} isDark={isDark}>
+        Ver favoritos
+      </Botao>
+    </Act>
+    {!!getRepos && <Hr></Hr>}
+  </>
 );
 
 export default Actions;
