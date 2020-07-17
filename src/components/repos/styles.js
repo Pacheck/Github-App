a689025a540fdcb310dc89624364d59126da8a8b
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-07-17 03:20:27 -0300

    Adjust css

--- src/components/repos/styles.js
+++ src/components/repos/styles.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 const StyledRepos = styled.div`
   display: flex;
+  flex: 1;
   flex-direction: column;
   flex-wrap: wrap;
   padding: 20px;
@@ -24,13 +25,19 @@ const StyledRepos = styled.div`
 
   a {
     display: flex;
+    flex-direction: row;
 
-    border: 1px solid #c3e2e3;
+    font-family: 'Rowdies', cursive;
+
+    ${(props) =>
+      props.isDark
+        ? 'background-color: #F3F4F6; border: solid 1px #b8babc; color: #000;'
+        : 'background-color: #1d2121; border: solid 1px #c3e2e3; color: #FFF;'}
+
     border-radius: 10px;
 
-    background-color: #1d2121;
-    color: #fff;
-    font-family: 'Rowdies', cursive;
+    /* background-color: #1d2121;
+    color: #fff; */
 
     padding: 20px;
     margin: 10px 0px;
