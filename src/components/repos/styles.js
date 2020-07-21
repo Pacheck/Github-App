7eb7b606584db79d106726086d843706a894b5f3
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-07-20 21:59:40 -0300

    Update styles.js

--- src/components/repos/styles.js
+++ src/components/repos/styles.js
@@ -48,10 +48,41 @@ const StyledRepos = styled.div`
     justify-content: center;
 
     :hover {
-      color: #000;
-      background-color: #d1caca;
-      transition: background-color 700ms;
-      transition: color 400ms;
+      // color: #000;  
+      // background-color: #d1caca;
+      // transition: background-color 700ms;
+      // transition: color 400ms;
+
+      background: linear-gradient(45deg, 
+        rgb(255, 0, 0), 
+        rgb(255, 115, 0), 
+        rgb(255, 251, 0), 
+        rgb(72, 255, 0), 
+        rgb(0, 255, 213), 
+        rgb(0, 43, 255), 
+        rgb(122, 0, 255),
+        rgb(255, 0, 200), 
+        rgb(255, 0, 0)
+      ) 0% 0  %  400%;
+
+      background-image: linear-gradient(45deg, 
+        rgb(255, 0, 0), 
+        rgb(255, 115, 0), 
+        rgb(255, 251, 0), 
+        rgb(72, 255, 0), 
+        rgb(0, 255, 213), 
+        rgb(0, 43, 255), 
+        rgb(122, 0, 255), 
+        rgb(255, 0, 200), 
+        rgb(255, 0, 0));
+
+        transition: opacity 0.3s ease-in-out 0s;
+        transition-property: opacity;
+        transition-duration: 0.3s;
+        transition-timing-function: ease-in-out;
+        transition-delay: 0s;
+
+        //End Hover
     }
 
     :focus {
